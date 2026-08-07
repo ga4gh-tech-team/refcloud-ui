e153358b8c8ef8d4536bbc511c5e9272446196d5
Jeremy Adams <jeremy.adams@ga4gh.org>
2026-08-07 11:26:34 -0400

    fix typo

--- pages/api/datasets/[id]/request-access.tsx
+++ pages/api/datasets/[id]/request-access.tsx
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(500).json({ message: 'Internal Server Error' })
     }
 
-    if (! sessionToken) {
+    if (!sessionToken) {
       return res.status(401).json({ message: 'Unauthorized' })
     }
 
